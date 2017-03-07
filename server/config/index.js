48a467dd4a1c10fc47d19abb4ba38287f23c0f2c
Edwin Moreno <moreno.edwinpr@gmail.com>
2017-03-07 13:14:59 -0500

    Added Handlebars as View Engine

--- server/config/index.js
+++ server/config/index.js
@@ -2,10 +2,10 @@
 
 var bodyParser = require("body-parser");
 var express = require('express');
+var exphbs = require('express-handlebars');
 
 var config = {
     registerAll: function(app){
-        //Enviroment Settings
 
 
         //Server Settings
@@ -14,6 +14,11 @@ var config = {
 };
 
 function expressSettings(app){
+    
+    //Set handlebars as view engine
+    app.set('views', 'server/views');
+    app.engine('.hbs', exphbs({defaultLayout: 'single',extname: '.hbs',layoutsDir: 'server/views/layouts',partialsDir:'server/views/partials/'}));
+    app.set('view engine', '.hbs');
     //Set Static Server
     app.use(express.static('client'));
     // parse application/json 
--- server/routes.js
+++ server/routes.js
@@ -7,6 +7,13 @@
 var routes = {
 
     registerAll: function(app){
+
+        //Default Route
+        app.get('/', function(req, res) {
+            res.render('index', {name:"Edwin"});
+        });
+
+        //Rest Api
         app.use('/api/things', require('./app/things'));
     }
 
